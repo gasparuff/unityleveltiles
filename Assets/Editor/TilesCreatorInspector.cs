6dcbb9bb812d96b02a029eabc18290de1db8d79a
Michael Gasparik <gasparuff@gmail.com>
2015-02-11 00:19:25 +0100

    Added support for more objects
    now supporting teleport and level-target objects

--- Assets/Editor/TilesCreatorInspector.cs
+++ Assets/Editor/TilesCreatorInspector.cs
@@ -1,21 +1,31 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using System.Collections;
 
 [CustomEditor(typeof(TilesCreator)), CanEditMultipleObjects]
 public class TilesCreatorInspector : Editor {
 
 
+	private TilesCreator tc;
+
 	public override void OnInspectorGUI () {
 		serializedObject.Update();
+		tc = (TilesCreator) serializedObject.targetObject;
 
+		
 		//Painting the GUI
 		EditorList.Show(serializedObject.FindProperty("tiles"), EditorListOption.ListLabel | EditorListOption.Buttons);
 		EditorList.Show(serializedObject.FindProperty("aliases"), EditorListOption.ListLabel | EditorListOption.Buttons);
+//		EditorList.Show(serializedObject.FindProperty("teleporters"), EditorListOption.ListLabel | EditorListOption.Buttons);
+		EditorGUILayout.PropertyField (serializedObject.FindProperty ("teleporter"));
+		EditorGUILayout.PropertyField (serializedObject.FindProperty ("target"));
+		tc.teleporterTag = EditorGUILayout.TextField("Teleporter Tag", tc.teleporterTag);
+		tc.targetTag = EditorGUILayout.TextField("Target Tag", tc.targetTag);
+
 		EditorGUILayout.PropertyField (serializedObject.FindProperty ("levelInput"));
 
 		//Here's where all the magic happens
 		if(GUILayout.Button("Build Object")){
-			TilesCreator tc = (TilesCreator) serializedObject.targetObject;
 
 			//Read the textfile
 			string text = tc.levelInput.text;
@@ -28,15 +38,18 @@ public class TilesCreatorInspector : Editor {
 			sizeOfFirstTile *= 2;
 
 			//Create a reference position where everything begins
-			Vector3 firstpos = new Vector3();
+			Vector3 firstpos = tc.transform.position;
 
 
 			//Prepare rowcounter
 			int rowsAdded = 0;
 
 			bool lastRowShifted = false;
-			bool shiftCurrentRow = false;
 
+			ArrayList teleportPositionArrayholder = new ArrayList();
+			ArrayList targetPositionArrayholder = new ArrayList();
+
+			
 			//Loop over every single row
 			foreach(string row in rows){
 
@@ -60,6 +73,8 @@ public class TilesCreatorInspector : Editor {
 
 					//Get the Array Index of the current tuple
 					string val = ""+chars[i]+chars[i+1];
+
+
 					int index = System.Array.IndexOf(tc.aliases, val);
 
 					//Making sure the tuple exists
@@ -77,19 +92,114 @@ public class TilesCreatorInspector : Editor {
 
 						//Add as child
 						instantiated.transform.parent = tc.transform;
+					}else{
+
+						if(val==tc.targetTag){
+							targetPositionArrayholder.Add(firstpos);
+						}else if(val==tc.teleporterTag){
+							teleportPositionArrayholder.Add(firstpos);
+						}
+
+
+//						GameObject go = getGameObjectFor(teleporterTag);
+//						if(go!=null){
+//							go.transform.position = firstpos;
+//							go.transform.parent = tc.transform;
+//						}
 					}
 
 					//Move the "cursor" ahead
 					firstpos.x += sizeOfFirstTile.x;
 				}
 
+//				teleportPositionArrayholder = getGameObjectAndPosition(teleportPositionArrayholder, teleporterTag);
+//				targetPositionArrayholder = getGameObjectAndPosition(targetPositionArrayholder, teleporterTag);
+
+				getGameObjectAndPosition (ref teleportPositionArrayholder, tc.teleporterTag);
+				getGameObjectAndPosition (ref targetPositionArrayholder, tc.targetTag);
+				//				
+//				if(targetPositionArrayholder.Count==4){
+//					Vector3 calculatedPosition = Vector3.zero;
+//					
+//					foreach(Vector3 pos in targetPositionArrayholder){
+//						calculatedPosition += pos;
+//					}
+//					calculatedPosition = calculatedPosition / targetPositionArrayholder.Count;
+//					GameObject go = getGameObjectFor(targetTag);
+//					if(go!=null){
+//						go.transform.position = calculatedPosition;
+//						go.transform.parent = tc.transform;
+//					}
+//					targetPositionArrayholder = new ArrayList();
+//				}
+				
 				//Move the "cursor" to the beginning of the next row
 				firstpos.z -= sizeOfFirstTile.z;
-				firstpos.x = 0.0f;
+				firstpos.x = tc.transform.position.x;
 				rowsAdded++;
 			}
 		}
 
+		if (GUILayout.Button ("Clear Tiles")) {
+			tc = (TilesCreator) serializedObject.targetObject;
+
+			int childs = tc.transform.childCount;
+			for (int i = childs - 1; i >= 0; i--)
+			{
+				GameObject.DestroyImmediate(tc.transform.GetChild(i).gameObject);
+			}
+		}
+
 		serializedObject.ApplyModifiedProperties();
+	}
+
+	void getGameObjectAndPosition (ref ArrayList teleportPositionArrayholder, string tag)
+	{
+		if (teleportPositionArrayholder.Count == 4) {
+			Vector3 calculatedPosition = Vector3.zero;
+			foreach (Vector3 pos in teleportPositionArrayholder) {
+				calculatedPosition += pos;
+			}
+			calculatedPosition = calculatedPosition / teleportPositionArrayholder.Count;
+			GameObject go = getGameObjectFor (tag);
+			if (go != null) {
+				go.transform.position = calculatedPosition;
+				go.transform.parent = tc.transform;
+			}
+			teleportPositionArrayholder = new ArrayList ();
+		}
+	}
+
+	private ArrayList getGameObjectAndPosition(ArrayList positions, string tag){
+		if(positions.Count==4){
+			Vector3 calculatedPosition = Vector3.zero;
+			
+			foreach(Vector3 pos in positions){
+				calculatedPosition += pos;
+			}
+			calculatedPosition = calculatedPosition / positions.Count;
+			GameObject go = getGameObjectFor(tc.targetTag);
+			if(go!=null){
+				go.transform.position = calculatedPosition;
+				go.transform.parent = tc.transform;
+			}
+			positions = new ArrayList();
+		}
+
+		return positions;
+
+	}
+
+	private GameObject getGameObjectFor(string tag){
+		if(tag==tc.teleporterTag){
+			GameObject instantiated = (GameObject) PrefabUtility.InstantiatePrefab(tc.teleporter);
+			return instantiated;
+		}
+		if(tag==tc.targetTag){
+			GameObject instantiated = (GameObject) PrefabUtility.InstantiatePrefab(tc.target);
+			return instantiated;
+		}
+
+		return null;
 	}
 }
--- Assets/Scripts/TilesCreator.cs
+++ Assets/Scripts/TilesCreator.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
 using System;
 
+[Serializable]
 public class TilesCreator : MonoBehaviour {
 
 	public GameObject[] tiles;
 	public String[] aliases;
 
+	public GameObject teleporter;
+	public GameObject target;
+
+	public string teleporterTag;
+	public string targetTag;
+
+	
 	public TextAsset levelInput;
 }
